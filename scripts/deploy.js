48ef8490ed8deaad480a0dfed5f7d172d08fb29a
Chinaka Chinedu <143916594+Neddie28@users.noreply.github.com>
2024-08-12 01:22:35 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,6 +8,7 @@ const hre = require("hardhat");
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  
   const unlockTime = currentTimestampInSeconds + 60;
 
   const lockedAmount = hre.ethers.parseEther("0.001");
